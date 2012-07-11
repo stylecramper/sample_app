68d887f3d3b01a4a2d4b71c0aec9fee09642a071
stylecramper <him@backattheranch.ca>
2012-07-11 13:38:13 -0700

    Added layout links with routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -47,5 +47,17 @@ render_views
       response.should have_selector('title', :content => "#{@base_title} | About Us")
     end
   end
+  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector('title', :content => "#{@base_title} | Help")
+    end
+  end
 
 end
