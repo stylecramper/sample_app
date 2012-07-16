51ac21eaaa6197f0831483369796dd2ce57d807e
Matt Anderson <him@backattheranch.ca>
2012-07-16 12:03:26 -0700

    Completed signin/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       # handle the save
+      sign_in @user
       redirect_to @user, :flash => { :success => 'Welcome to the Sample App!' } 
     else
       @title = 'Sign up'
